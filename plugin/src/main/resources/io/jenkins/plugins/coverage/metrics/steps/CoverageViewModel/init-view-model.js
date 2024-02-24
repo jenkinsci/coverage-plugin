12dc03824f24ee3942d107539c9c14001f6f451a
Ulli Hafner <ullrich.hafner@gmail.com>
2024-02-24 19:42:56 +0100

    Fix name of `proxy` variable.

--- plugin/src/main/resources/io/jenkins/plugins/coverage/metrics/steps/CoverageViewModel/init-view-model.js
+++ plugin/src/main/resources/io/jenkins/plugins/coverage/metrics/steps/CoverageViewModel/init-view-model.js
@@ -1,8 +1,10 @@
+/* global jQuery3, proxy, echartsJenkinsApi, bootstrap5 */
+
 (function () {
     proxy.getJenkinsColorIDs(function (colors) {
         const jenkinsColors = getJenkinsColors(colors.responseObject());
         const colorJson = JSON.stringify(Object.fromEntries(jenkinsColors));
         proxy.setJenkinsColors(colorJson);
-        new CoverageChartGenerator(jQuery3).populateDetailsCharts(jenkinsColors);
+        new CoverageChartGenerator(jQuery3, proxy).populateDetailsCharts(jenkinsColors);
     });
 })();
