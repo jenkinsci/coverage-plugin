739ec2139e5ed3773332acf43e560c5d79b1e2bd
Ulli Hafner <ullrich.hafner@gmail.com>
2025-04-27 22:26:50 +0200

    Use different text colors for some colors based on WCAG luminance

--- plugin/src/main/resources/io/jenkins/plugins/coverage/metrics/steps/CoverageViewModel/view-model.js
+++ plugin/src/main/resources/io/jenkins/plugins/coverage/metrics/steps/CoverageViewModel/view-model.js
@@ -1,7 +1,42 @@
-/* global jQuery3, proxy, echartsJenkinsApi, bootstrap5, culori */
+/* global $, proxy, echartsJenkinsApi, bootstrap5 */
 
-const CoverageChartGenerator = function ($, proxy) { // NOPMD
-    let selectedTreeNode;
+(function ($) {
+    function fillDialog(trendConfiguration, jsonConfiguration) {
+        const metrics = jsonConfiguration['metrics'];
+        if (metrics) {
+            Object.entries(metrics).forEach(([metric, isChecked]) => {
+                trendConfiguration.find(`input[type="checkbox"][name="${metric}"]`).prop('checked', isChecked);
+            });
+        }
+        const useLines = jsonConfiguration['useLines'];
+        if (useLines) {
+            trendConfiguration.find('input[type="checkbox"][name="lines"]').prop('checked', useLines);
+        }
+    }
+
+    function saveDialog(trendConfiguration) {
+        const metrics = {};
+
+        trendConfiguration.find('input[type="checkbox"][id*="-history-metric"]').each(function () {
+            metrics[$(this).attr('name')] = $(this).prop('checked');
+        });
+        return {
+            'metrics': metrics,
+            'useLines': trendConfiguration.find('input[name=lines]').prop('checked')
+        };
+    }
+
+    function getJenkinsColors(colors) { // NOPMD
+        // TODO: also handle HSL colors and parse them to hex in order to use dark mode colors
+        const colorHexMapping = new Map;
+        colors.forEach(function (jenkinsId) {
+            const colorHex = getComputedStyle(document.body).getPropertyValue(jenkinsId);
+            if (colorHex.match(/^#[a-fA-F0-9]{6}$/) !== null) {
+                colorHexMapping.set(jenkinsId, colorHex);
+            }
+        })
+        return colorHexMapping;
+    }
 
     function resolveJenkinsColor(colorName) {
         return echartsJenkinsApi.resolveJenkinsColor(colorName);
@@ -219,10 +254,20 @@ const CoverageChartGenerator = function ($, proxy) { // NOPMD
             if (node.itemStyle && isLeafWithValue(node, leaf)) {
                 const value = getValueToColorize(node);
                 const ratio = (value - min) / (max - min);
-                const color = culori.formatHex(colors(ratio));
+                const backgroundColor = colors(ratio);
+                const color = culori.formatHex(backgroundColor);
 
                 node.itemStyle.color = color;
                 node.itemStyle.borderColor = color;
+
+                if (culori.wcagLuminance(backgroundColor) > 0.230) { // WCAG 2 ratio is 0.179, probably will be changed in the future...
+                    node.label.color = '--black';
+                    node.upperLabel.color = '--black';
+                }
+                else {
+                    node.label.color = '--white';
+                    node.upperLabel.color = '--white';
+                }
             }
 
             if (Array.isArray(node.children)) {
@@ -324,7 +369,6 @@ const CoverageChartGenerator = function ($, proxy) { // NOPMD
                     for (let i = 2; i < treePathInfo.length; i++) {
                         treePath.push(treePathInfo[i].name);
                     }
-                    selectedTreeNode = info.id; // NOPMD
                     const values = info.value;
                     const total = values[0];
                     const tooltip = values[1];
@@ -374,189 +418,196 @@ const CoverageChartGenerator = function ($, proxy) { // NOPMD
         treeChart.resize();
     }
 
-    this.populateDetailsCharts = function (jenkinsColors) {
+    /**
+     * Activate the tab that has been visited the last time. If there is no such tab, highlight the first one.
+     * If the user selects the tab using an #anchor prefer this tab.
+     */
+    function registerTabEvents() {
         /**
-         * Activate the tab that has been visited the last time. If there is no such tab, highlight the first one.
-         * If the user selects the tab using an #anchor prefer this tab.
+         * Activates the specified tab.
+         *
+         * @param {String} selector - selector of the tab
+         * @return true if the tab has been selected
          */
-        function registerTabEvents() {
-            /**
-             * Activates the specified tab.
-             *
-             * @param {String} selector - selector of the tab
-             * @return true if the tab has been selected
-             */
-            function selectTab(selector) {
-                const detailsTabs = $('#tab-details');
-                const selectedTab = detailsTabs.find(selector);
+        function selectTab(selector) {
+            const detailsTabs = $('#tab-details');
+            const selectedTab = detailsTabs.find(selector);
 
-                if (selectedTab.length !== 0) {
-                    const tab = new bootstrap5.Tab(selectedTab[0]);
-                    tab.show();
+            if (selectedTab.length !== 0) {
+                const tab = new bootstrap5.Tab(selectedTab[0]);
+                tab.show();
 
-                    return true;
-                }
-                return false
+                return true;
             }
+            return false
+        }
 
-            const selectedTabID = 'jenkins-coverage-activeTab';
-            const url = document.location.toString();
-            if (url.match('#')) {
-                window.location.hash = '';
-                const tabName = url.split('#')[1];
-                if (selectTab('a[data-bs-target="#' + tabName + '"]')) {
-                    localStorage.setItem(selectedTabID, '#' + tabName);
-                }
+        const selectedTabID = 'jenkins-coverage-activeTab';
+        const url = document.location.toString();
+        if (url.match('#')) {
+            window.location.hash = '';
+            const tabName = url.split('#')[1];
+            if (selectTab('a[data-bs-target="#' + tabName + '"]')) {
+                localStorage.setItem(selectedTabID, '#' + tabName);
+            }
+        }
+        else {
+            const activeTab = localStorage.getItem(selectedTabID);
+            if (activeTab) {
+                selectTab('a[data-bs-target="' + activeTab + '"]');
+            }
+        }
+        if ($('#tab-details a.active').length === 0) {
+            selectTab('li:first-child a'); // fallback if all other options fail
+        }
+
+        $('a[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
+            window.location.hash = e.target.hash;
+            const activeTab = $(e.target).attr('data-bs-target');
+            localStorage.setItem(selectedTabID, activeTab);
+            redrawCharts();
+        });
+    }
+
+    /**
+     * Loads all chart JSON models via AJAX calls from the server and renders the corresponding echarts.
+     */
+    function initializeCharts() {
+        renderCoverageTrendChart();
+        renderMetricsTrendChart();
+
+        if (document.querySelector('#coverage-overview')) {
+            proxy.getOverview(function (t) {
+                createOverview(t.responseObject(), 'coverage-overview');
+            });
+        }
+
+        $('.tree-chart').each(function () {
+            const id = $(this).attr('id');
+            const name = $(this).attr('data-item-name');
+            const isAscending = $(this).attr('data-item-order') === "LARGER_IS_BETTER";
+            const isCoverage = $(this).attr('data-item-coverage') === "true";
+            proxy.getCoverageTree(id, function (t) {
+                createFilesTreeMap(t.responseObject(), id, name, isAscending, isCoverage);
+            });
+        });
+    }
+
+    function resizeChartOf(selector) {
+        if (document.querySelector(selector)) {
+            $(selector)[0].echart.resize();
+        }
+    }
+
+    function renderCoverageTrendChart() {
+        if ($('#coverage-trend').is(':visible')) {
+            const configuration = JSON.stringify(echartsJenkinsApi.readFromLocalStorage('jenkins-echarts-chart-configuration-coverage-history'));
+            proxy.getTrendChart(configuration, function (t) {
+                echartsJenkinsApi.renderConfigurableZoomableTrendChart('coverage-trend', t.responseJSON, 'chart-configuration-coverage-history', openBuild);
+                resizeChartOf('#coverage-trend');
+            });
+        }
+    }
+
+    function renderMetricsTrendChart() {
+        if ($('#metrics-trend').is(':visible')) {
+            const configuration = JSON.stringify(echartsJenkinsApi.readFromLocalStorage('jenkins-echarts-chart-configuration-metrics-history'));
+            proxy.getMetricsTrendChart(configuration, function (t) {
+                echartsJenkinsApi.renderConfigurableZoomableTrendChart('metrics-trend', t.responseJSON, 'chart-configuration-metrics-history', openBuild);
+                resizeChartOf('#metrics-trend');
+            });
+        }
+    }
+
+    /**
+     * Event handler to resize all charts.
+     */
+    function redrawCharts() {
+        renderCoverageTrendChart();
+        renderMetricsTrendChart();
+
+        resizeChartOf('#coverage-overview');
+
+        $('.tree-chart').each(function () {
+            $(this)[0].echart.resize();
+        });
+    }
+
+    function registerCoverageChartConfiguration() {
+        const trendConfigurationDialogId = 'chart-configuration-coverage-history';
+        $('#' + trendConfigurationDialogId).on('hidden.bs.modal', function () {
+            redrawCharts();
+        });
+    }
+
+    function registerMetricsChartConfiguration() {
+        const trendConfigurationDialogId = 'chart-configuration-metrics-history';
+        $('#' + trendConfigurationDialogId).on('hidden.bs.modal', function () {
+            redrawCharts();
+        });
+    }
+
+    /**
+     * Initializes a selection listener for a datatable which loads the selected source code.
+     *
+     * @param {String} tableId The ID of the DataTable
+     */
+    function initializeSourceCodeSelection(tableId) {
+        const datatable = $('#' + tableId + '-table-inline').DataTable();
+        const sourceView = $('#' + tableId + '-source-file');
+        const noFileSelectedBanner = $('#' + tableId + '-no-selection');
+        const noSourceAvailableBanner = $('#' + tableId + '-no-source');
+
+        function showNoSelection() {
+            sourceView.hide();
+            noSourceAvailableBanner.hide();
+            noFileSelectedBanner.show();
+        }
+
+        function showNoSourceCode() {
+            sourceView.hide();
+            noFileSelectedBanner.hide();
+            noSourceAvailableBanner.show();
+        }
+
+        function showSourceCode() {
+            noFileSelectedBanner.hide();
+            noSourceAvailableBanner.hide();
+            sourceView.show();
+        }
+
+        showNoSelection();
+        datatable.on('select', function (e, dt, type, indexes) {
+            if (type === 'row') {
+                showSourceCode();
+                sourceView.html('Loading...');
+                const rowData = datatable.rows(indexes).data().toArray();
+                proxy.getSourceCode(rowData[0].fileHash, tableId + '-table', function (t) {
+                    const sourceCode = t.responseObject();
+                    if (sourceCode === "n/a") {
+                        showNoSourceCode();
+                    }
+                    else {
+                        sourceView.html(sourceCode);
+                    }
+                });
             }
             else {
-                const activeTab = localStorage.getItem(selectedTabID);
-                if (activeTab) {
-                    selectTab('a[data-bs-target="' + activeTab + '"]');
-                }
-            }
-            if ($('#tab-details a.active').length === 0) {
-                selectTab('li:first-child a'); // fallback if all other options fail
-            }
-
-            $('a[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
-                window.location.hash = e.target.hash;
-                const activeTab = $(e.target).attr('data-bs-target');
-                localStorage.setItem(selectedTabID, activeTab);
-                redrawCharts();
-            });
-        }
-
-        /**
-         * Loads all chart JSON models via AJAX calls from the server and renders the corresponding echarts.
-         */
-        function initializeCharts() {
-            renderCoverageTrendChart();
-            renderMetricsTrendChart();
-
-            if (document.querySelector('#coverage-overview')) {
-                proxy.getOverview(function (t) {
-                    createOverview(t.responseObject(), 'coverage-overview');
-                });
-            }
-
-            $('.tree-chart').each(function () {
-                const id = $(this).attr('id');
-                const name = $(this).attr('data-item-name');
-                const isAscending = $(this).attr('data-item-order') === "LARGER_IS_BETTER";
-                const isCoverage = $(this).attr('data-item-coverage') === "true";
-                proxy.getCoverageTree(id, function (t) {
-                    createFilesTreeMap(t.responseObject(), id, name, isAscending, isCoverage);
-                });
-            });
-        }
-
-        function resizeChartOf(selector) {
-            if (document.querySelector(selector)) {
-                $(selector)[0].echart.resize();
-            }
-        }
-
-        function renderCoverageTrendChart() {
-            if ($('#coverage-trend').is(':visible')) {
-                const configuration = JSON.stringify(echartsJenkinsApi.readFromLocalStorage('jenkins-echarts-chart-configuration-coverage-history'));
-                proxy.getTrendChart(configuration, function (t) {
-                    echartsJenkinsApi.renderConfigurableZoomableTrendChart('coverage-trend', t.responseJSON, 'chart-configuration-coverage-history', openBuild);
-                    resizeChartOf('#coverage-trend');
-                });
-            }
-        }
-
-        function renderMetricsTrendChart() {
-            if ($('#metrics-trend').is(':visible')) {
-                const configuration = JSON.stringify(echartsJenkinsApi.readFromLocalStorage('jenkins-echarts-chart-configuration-metrics-history'));
-                proxy.getMetricsTrendChart(configuration, function (t) {
-                    echartsJenkinsApi.renderConfigurableZoomableTrendChart('metrics-trend', t.responseJSON, 'chart-configuration-metrics-history', openBuild);
-                    resizeChartOf('#metrics-trend');
-                });
-            }
-        }
-
-        /**
-         * Event handler to resize all charts.
-         */
-        function redrawCharts() {
-            renderCoverageTrendChart();
-            renderMetricsTrendChart();
-
-            resizeChartOf('#coverage-overview');
-
-            $('.tree-chart').each(function () {
-                $(this)[0].echart.resize();
-            });
-        }
-
-        function registerCoverageChartConfiguration() {
-            const trendConfigurationDialogId = 'chart-configuration-coverage-history';
-            $('#' + trendConfigurationDialogId).on('hidden.bs.modal', function () {
-                redrawCharts();
-            });
-        }
-
-        function registerMetricsChartConfiguration() {
-            const trendConfigurationDialogId = 'chart-configuration-metrics-history';
-            $('#' + trendConfigurationDialogId).on('hidden.bs.modal', function () {
-                redrawCharts();
-            });
-        }
-
-        /**
-         * Initializes a selection listener for a datatable which loads the selected source code.
-         *
-         * @param {String} tableId The ID of the DataTable
-         */
-        function initializeSourceCodeSelection(tableId) {
-            const datatable = $('#' + tableId + '-table-inline').DataTable();
-            const sourceView = $('#' + tableId + '-source-file');
-            const noFileSelectedBanner = $('#' + tableId + '-no-selection');
-            const noSourceAvailableBanner = $('#' + tableId + '-no-source');
-
-            function showNoSelection() {
-                sourceView.hide();
-                noSourceAvailableBanner.hide();
-                noFileSelectedBanner.show();
-            }
-
-            function showNoSourceCode() {
-                sourceView.hide();
-                noFileSelectedBanner.hide();
-                noSourceAvailableBanner.show();
-            }
-
-            function showSourceCode() {
-                noFileSelectedBanner.hide();
-                noSourceAvailableBanner.hide();
-                sourceView.show();
-            }
-
-            showNoSelection();
-            datatable.on('select', function (e, dt, type, indexes) {
-                if (type === 'row') {
-                    showSourceCode();
-                    sourceView.html('Loading...');
-                    const rowData = datatable.rows(indexes).data().toArray();
-                    proxy.getSourceCode(rowData[0].fileHash, tableId + '-table', function (t) {
-                        const sourceCode = t.responseObject();
-                        if (sourceCode === "n/a") {
-                            showNoSourceCode();
-                        }
-                        else {
-                            sourceView.html(sourceCode);
-                        }
-                    });
-                }
-                else {
-                    showNoSelection();
-                }
-            })
-            datatable.on('deselect', function () {
                 showNoSelection();
-            });
-        }
+            }
+        })
+        datatable.on('deselect', function () {
+            showNoSelection();
+        });
+    }
+
+    echartsJenkinsApi.configureChart('coverage-history', fillDialog, saveDialog);
+    echartsJenkinsApi.configureChart('metrics-history', fillDialog, saveDialog);
+
+    proxy.getJenkinsColorIDs(function (colors) {
+        const jenkinsColors = getJenkinsColors(colors.responseObject());
+        const colorJson = JSON.stringify(Object.fromEntries(jenkinsColors));
+        proxy.setJenkinsColors(colorJson);
 
         registerCoverageChartConfiguration();
         registerMetricsChartConfiguration();
@@ -580,7 +631,7 @@ const CoverageChartGenerator = function ($, proxy) { // NOPMD
 
         $(document).ready(function () {
             initializeSourceCodeSelection('absolute-coverage');
-            initializeSourceCodeSelection('change-coverage');
+            initializeSourceCodeSelection('modified-lines-coverage');
             initializeSourceCodeSelection('indirect-coverage');
 
             $('input[id ^= "changed"]').on('change', function () {
@@ -591,5 +642,5 @@ const CoverageChartGenerator = function ($, proxy) { // NOPMD
                 });
             });
         });
-    }
-};
+    });
+})(jQuery3);
